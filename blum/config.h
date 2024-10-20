750b00ee7b401e29991032dabbdae818334454e4
eliseydudin <dudinelisey630@gmail.com>
2024-10-20 14:39:07 +0300

    _WIN64 was not defined? this might work

--- blum/config.h
+++ blum/config.h
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#ifndef _WIN32
+#if !defined(_WIN32) || !(defined(_WIN64))
   #define B_EXPORT __attribute__((visibility("default")))
 #else
   #define __declspec(dllexport)
