df3274a42754692cf41b514195401099c949e109
eliseydudin <dudinelisey630@gmail.com>
2024-10-20 14:14:08 +0300

    more numeric types in nums.h

--- blum/nums.h
+++ blum/nums.h
@@ -8,6 +8,14 @@ typedef uint32_t u32;
 typedef uint16_t u16;
 typedef uint8_t u8;
 
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t i8;
+
+typedef float f32;
+typedef double f64;
+
 #ifndef bool
 typedef u8 bool;
   #define true 1
