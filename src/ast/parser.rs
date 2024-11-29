356c12e6445db8b736757c7004932a8f428be55a
eliseydudin <dudinelisey630@gmail.com>
2024-11-29 11:29:42 +0300

    adding stubs for the parser

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -1,5 +1,23 @@
-pub struct Parser {}
+use super::{Lexer, Token};
+
+pub struct Parser {
+    pub tokens: Vec<Token>,
+    pub ast: Vec<()>,
+    pub position: usize,
+}
 
 impl Parser {
-    pub fn new(input: impl Into<String>) {}
+    pub fn new(input: impl Into<String>) -> Self {
+        let input: String = input.into();
+
+        let tokens = Lexer::new(input.chars()).parse().finish();
+        let ast = vec![];
+        let position = 0usize;
+
+        Self {
+            tokens,
+            ast,
+            position,
+        }
+    }
 }
