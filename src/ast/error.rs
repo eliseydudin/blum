1d433d7f3d63dc4f8554fae73c586be05dcf7060
eliseydudin <dudinelisey630@gmail.com>
2024-12-01 11:43:26 +0300

    better error

--- src/ast/error.rs
+++ src/ast/error.rs
@@ -1,63 +1,35 @@
-use super::TokenType;
+use crate::ast::TokenType;
 use std::{error::Error as ErrorTrait, fmt::Display};
 
 #[derive(Debug)]
-pub enum Error {
-    Todo,
-    Message(String),
-    Expect {
+pub enum ErrorKind {
+    UnexpectedToken {
         expected: TokenType,
         found: TokenType,
     },
-    Eof(TokenType),
 }
 
-impl Error {
-    pub fn new(s: impl Into<String>) -> Self {
-        Self::Message(s.into())
-    }
-
-    pub const fn todo() -> Self {
-        Self::Todo
-    }
-
-    pub fn wrap<T>(self) -> Result<T> {
-        Err(self)
-    }
+#[derive(Debug)]
+pub struct Error {
+    kind: ErrorKind,
+    position: usize,
 }
 
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Todo => write!(f, "error: not yet implemented"),
-            Self::Expect { expected, found } => write!(
-                f,
-                "error: expected token \"{:?}\", found: \"{:?}\"",
-                expected, found
-            ),
-            Self::Message(m) => write!(f, "error: {m}"),
-            Self::Eof(e) => write!(f, "error: expected {e:?}, found end of file"),
+        match &self.kind {
+            ErrorKind::UnexpectedToken { expected, found } => {
+                write!(
+                    f,
+                    "error (at {}): expected {expected:?}, found {found:?}",
+                    self.position
+                )
+            }
         }
     }
 }
 
-impl ErrorTrait for Error {
-    fn source(&self) -> Option<&(dyn ErrorTrait + 'static)> {
-        None
-    }
-}
-
-pub type Result<T> = std::result::Result<T, Error>;
-
-#[macro_export]
-macro_rules! error {
-    () => {
-        $crate::ast::Error::todo()
-    };
-    ($msg:tt) => {
-        $crate::ast::Error::new(format!("{}", $msg))
-    };
-}
+impl ErrorTrait for Error {}
 
 /*
 () => {
