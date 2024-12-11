c177c0ee9892e4651d07ad08e51d76f4f8a2f886
eliseydudin <dudinelisey630@gmail.com>
2024-12-11 12:12:20 +0300

    rewriting the parser attempt #3

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -1,13 +1,1 @@
-pub mod error;
-pub mod expr;
-pub mod lexer;
-pub mod parser;
-pub mod token;
-pub mod token_iter;
 
-pub use error::*;
-pub use expr::Expr;
-pub use lexer::Lexer;
-pub use parser::Parser;
-pub use token::{Operand, Token, TokenType};
-pub use token_iter::TokenIter;
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,3 @@
 pub mod ast;
-use ast::Parser;
 
-fn main() {
-    let buff = std::fs::read_to_string("test.blum").unwrap();
-    let mut parser = Parser::new(buff);
-    println!("{:#?}", parser.tokens);
-    let errors = parser.parse();
-
-    for error in errors {
-        println!("{error}");
-    }
-
-    println!("{:#?}", parser.ast)
-}
+fn main() {}
