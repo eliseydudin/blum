ae21ac415cef3ff6a0c40812958c61e72810fdfb
eliseydudin <dudinelisey630@gmail.com>
2024-12-13 14:33:26 +0300

    try_let

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -1,4 +1,8 @@
-use super::{expr, statement as stmt, token as tk, token_stream::TokenStream};
+use super::{
+    expr, statement as stmt,
+    token::{self as tk, TokenType},
+    token_stream::TokenStream,
+};
 
 pub struct Parser {
     tokens: Vec<tk::Token>,
@@ -24,8 +28,31 @@ impl Parser {
         self.tokens.get(self.current + 1).cloned()
     }
 
+    pub fn expect(&mut self, ttype: TokenType) -> Option<tk::Token> {
+        let tk = self.advance();
+        tk.and_then(|t| if t.ttype == ttype { Some(t) } else { None })
+    }
+
     pub fn advance(&mut self) -> Option<tk::Token> {
         self.current += 1;
         self.tokens.get(self.current - 1).cloned()
+    }
+
+    pub fn read_expression(&mut self) -> Option<expr::Expr> {
+        todo!()
+    }
+
+    pub fn try_let(&mut self) -> Option<stmt::Let> {
+        let _bind = self.expect(TokenType::Let)?;
+        let name = self.expect(TokenType::Identifier)?;
+        let _eq = self.expect(TokenType::Equal)?;
+        let value = self.read_expression()?;
+
+        let let_statement = stmt::Let {
+            name: name.lexeme,
+            value,
+        };
+
+        Some(let_statement)
     }
 }
